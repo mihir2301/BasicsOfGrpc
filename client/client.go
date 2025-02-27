7274206ba8c4f7ce069ad9d3e3b1a6949d524c4d
MIHIR GUPTA <Mihir Gupta>
2025-02-27 19:25:33 +0530

    Rest api included

--- client/client.go
+++ client/client.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	protoc "grpc/protoc"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+var client protoc.ExampleClient
 
 func main() {
 	//connection to internal gpc server
@@ -14,9 +18,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	client := protoc.NewExampleClient(conn)
+	client = protoc.NewExampleClient(conn)
 
-	req := &protoc.HelloRequest{SomeString: "Hello from client"}
+	r := gin.Default()
+	r.GET("/sent-message/:message", clientConnection)
+	r.Run(":8080")
+}
+
+func clientConnection(c *gin.Context) {
+	message := c.Param("message")
+
+	req := &protoc.HelloRequest{SomeString: message}
 
 	client.ServeReply(context.TODO(), req)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "message sent successfully to serve:= " + message,
+	})
 }
